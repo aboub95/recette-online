e5e0b387620ccf069dc8f3707260dbd85c07d5d6
aboub95 <messireabou33@gmail.com>
2024-11-22 17:30:37 +0100

    First commit by Abou95

---
+++ app/components/Accueil/Accueil.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Accueil() {
+  return (
+    <div>
+      <h1 className="h-screen bg-gray-500">Bienvenue sur la page d'accueil</h1>
+    </div>
+  );
+}
---
+++ app/components/Navbar.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Nav() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-orange-300 text-center font-bold">My nav</h1>
+    </div>
+  );
+}
